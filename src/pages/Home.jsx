3760bd76d32a8a517e5ecd2c3ebc1eab9e70a305
terlanmesimov <terlanmesimow@gmail.com>
2023-09-30 20:10:29 +0400

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,16 @@ import { Link } from "react-router-dom";
 // COMPONENTS
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+// IMAGES
+import heroBg from "../assets/images/hero_bg.jpeg"
 
 const Home = () => {
   return (
     <>
       <section className="hero">
+        <div className="hero_bg">
+          <img src={heroBg} alt="" />
+        </div>
         <div className="content">
           <h1 className="hero_heading">WE'RE PLAYING AWAY!</h1>
           <h2 className="hero_heading">SUPPORT THE TEAM WHEREVER THEY GO</h2>
