1228dffa70a6411c13aa0b06a1212a1a622b4e1c
terlanmesimov <terlanmesimow@gmail.com>
2023-10-06 00:19:24 +0400

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 // IMAGES
 import heroBg from "../assets/images/hero_bg.jpeg";
+import cardFirstImage from "../assets/images/card_image_first.jpeg";
+import cardSecondImage from "../assets/images/card_image_second.jpeg";
 
 const Home = () => {
   return (
@@ -19,7 +21,34 @@ const Home = () => {
       </section>
       <section className="shopify_section">
         <div className="container">
-          <div className="row"></div>
+          <div className="row">
+            <div className="title">
+              <div className="title_text">
+                <h2>2ND AND THIRD KITS</h2>
+              </div>
+              <div className="diogonal_lines deg45"></div>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <div className="card_image">
+                  <div className="first_image">
+                    <img src={cardFirstImage} alt="first img" />
+                  </div>
+                  <div className="second_image">
+                    <img src={cardSecondImage} alt="second img" />
+                  </div>
+                </div>
+                <div className="card_features">
+                  <div className="content">
+                    <p className="card_info">
+                      Mens Away Authentic Shirt 23/24 Navy
+                    </p>
+                    <span className="price">A partir de 150.00€</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
