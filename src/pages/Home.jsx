5704963b28f535c12febf2ae6add3bf89cbba771
terlanmesimov <terlanmesimow@gmail.com>
2023-10-07 01:32:00 +0400

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
-// IMAGES
+// IMAGES (Hero)
 import heroBg from "../assets/images/hero_bg.jpeg";
+// IMAGES (Shopify)
 import cardFirstImage from "../assets/images/card_image_first.jpeg";
 import cardSecondImage from "../assets/images/card_image_second.jpeg";
+// IMAGES (Collections)
+import collectionImageOne from "../assets/images/collections_image_1.jpeg";
+import collectionImageTwo from "../assets/images/collections_image_2.jpeg";
+import collectionImageThree from "../assets/images/collections_image_3.jpeg";
+import collectionImageFour from "../assets/images/collections_image_4.jpeg";
 
 const Home = () => {
   return (
@@ -209,7 +215,72 @@ const Home = () => {
           </div>
         </div>
       </section>
-      
+      <section className="collections_section">
+        <div className="container">
+          <div className="row">
+            <div className="title">
+              <div className="title_text">
+                <h2>KITS</h2>
+              </div>
+              <div className="diogonal_lines deg45"></div>
+            </div>
+            <div className="collections">
+              <div className="collection_item">
+                <div className="card">
+                  <div className="card_image">
+                    <img src={collectionImageOne} alt="collection_image" />
+                  </div>
+                  <div className="card_info">
+                    <h2>23/24 REAL MADRID HOME KITS</h2>
+                    <Link to="/">
+                      <span>SHOP NOW</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="collection_item">
+                <div className="card">
+                  <div className="card_image">
+                    <img src={collectionImageTwo} alt="collection_image" />
+                  </div>
+                  <div className="card_info">
+                    <h2>23/24 REAL MADRID AWAY KITS</h2>
+                    <Link to="/">
+                      <span>SHOP NOW</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="collection_item">
+                <div className="card">
+                  <div className="card_image">
+                    <img src={collectionImageThree} alt="collection_image" />
+                  </div>
+                  <div className="card_info">
+                    <h2>THIRD KIT REAL MADRID 23/24</h2>
+                    <Link to="/">
+                      <span>SHOP NOW</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="collection_item">
+                <div className="card">
+                  <div className="card_image">
+                    <img src={collectionImageFour} alt="collection_image" />
+                  </div>
+                  <div className="card_info">
+                    <h2>23/24 REAL MADRID GOALKEEPER KITS</h2>
+                    <Link to="/">
+                      <span>SHOP NOW</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
