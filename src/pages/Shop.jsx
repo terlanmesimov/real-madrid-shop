5b6008fb79afee60da55de10096845dc994bc28a
terlanmesimov <terlanmesimow@gmail.com>
2023-10-10 22:08:43 +0400

    commit

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -113,6 +113,10 @@ const Shop = () => {
               <Product />
               <Product />
               <Product />
+              <Product />
+              <Product />
+              <Product />
+              <Product />
             </div>
           </div>
         </div>
