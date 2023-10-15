6f075c3c79b281fdb42fa916c0d1c53127ab740d
terlanmesimov <terlanmesimow@gmail.com>
2023-10-16 00:34:16 +0400

    commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+// ICON
+import ErrorOutlineOutlinedIcon from "@mui/icons-material/ErrorOutlineOutlined";
 
 const Register = () => {
+  const [focusInput, setFocusInput] = useState(false);
   useEffect(() => {
     document.title = "Real Madrid CF | Log in or access";
     document.head.innerHTML += `<link rel="shortcut icon" href="favicon-2.ico" type="image/x-icon" />`;
@@ -20,7 +23,31 @@ const Register = () => {
           <h3>Welcome!</h3>
           <p>Sign in or create your account</p>
         </div>
-        <form className="form"></form>
+        <form className="form">
+          <div className="email input_content">
+            <input
+              type="email"
+              className="email_input input"
+              onFocus={() => setFocusInput(!focusInput)}
+              onBlur={() => setFocusInput(!focusInput)}
+            />
+            <span className="info_icon">
+              <ErrorOutlineOutlinedIcon />
+            </span>
+            <label
+              htmlFor="email"
+              className={`email_label label ${focusInput ? "active" : ""}`}
+            >
+              Email / Socio No. / Madridista No
+            </label>
+            <span className="error error_email">
+              Please, enter a valid email (for example: you@example.com)
+            </span>
+          </div>
+          <button className="form_btn">
+            <span>Continue</span>
+          </button>
+        </form>
       </div>
       <div className="register_footer"></div>
     </div>
