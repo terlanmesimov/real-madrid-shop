9f0735290aa5ffda7300e84e4071524810925974
terlanmesimov <terlanmesimow@gmail.com>
2023-11-04 05:58:09 +0400

    commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,39 @@
 import { useTranslation } from "react-i18next";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { MainContext } from "../utils/MainContext";
 // IMAGES
 import searchIcon from "../assets/images/icons8-search-50.png";
 
 const SearchBar = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const [openList, setOpenList] = useState(false);
+  const { products } = useContext(MainContext);
+
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    setOpenList(false);
+  }, [window.scrollY]);
+
+  const performSearch = (searchQuery) => {
+    const filteredResults = products.filter((item) =>
+      item.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setResults(filteredResults);
+    console.log(results);
+  };
+
+  const handleInputChange = (event) => {
+    setOpenList(true);
+    const newQuery = event.target.value;
+    setQuery(newQuery);
+    performSearch(newQuery);
+  };
+
   return (
     <div className="search_bar">
       <div className="form_content">
@@ -12,6 +42,8 @@ const SearchBar = () => {
             type="text"
             className="search_input"
             placeholder={t("header.search")}
+            value={query}
+            onChange={handleInputChange}
             onFocus={(e) => {
               e.target.placeholder = "";
             }}
@@ -24,6 +56,32 @@ const SearchBar = () => {
           </button>
         </form>
       </div>
+      {openList && (
+        <div className="results">
+          <ul className="result_list">
+            {results.map((item) => {
+              return (
+                <li
+                  key={item.id}
+                  id={item.id}
+                  className="list_item"
+                  onClick={() => {
+                    navigate(`/product-details/${item.id}`);
+                    window.location.reload();
+                  }}
+                >
+                  <img
+                    className="item_image"
+                    src={`${process.env.REACT_APP_DOMAIN}/${item.productImage}`}
+                    alt="item"
+                  />
+                  <h3 className="item_name">{item.name}</h3>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
